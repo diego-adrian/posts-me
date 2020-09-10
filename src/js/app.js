fc88fd9dba326ed951642f949113d07403902f8d
adrian <adry.dabp@gmail.com>
2020-09-09 21:26:28 -0400

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -48,6 +48,38 @@ const sendData = async (e) => {
     Message('error').MaterialSnackbar.showSnackbar(data);
   }
 };
+const createPosts = ({ description, title }) => {
+  const mainContent = document.createElement('div');
+  mainContent.className = 'card-wide mdl-card mdl-shadow--2dp';
+
+  const cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  const h2 = document.createElement('h2');
+  h2.className = 'mdl-card__title-text';
+  h2.appendChild(document.createTextNode(title));
+  cardTitle.appendChild(h2);
+
+  const cardText = document.createElement('div');
+  cardText.className = 'mdl-card__supporting-text';
+  cardText.appendChild(document.createTextNode(description));
+  
+  const btnShare = document.createElement('div');
+  btnShare.className = 'mdl-card__menu';
+  const btn = document.createElement('button');
+  btn.className = 'mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect';
+  const icon = document.createElement('i');
+  icon.className = 'material-icons';
+  icon.appendChild(document.createTextNode('share'));
+
+  btn.appendChild(icon);
+  btnShare.appendChild(btn);
+
+  mainContent.appendChild(cardTitle);
+  mainContent.appendChild(cardText);
+  mainContent.appendChild(btnShare);
+  
+  MAIN.appendChild(mainContent);
+};
 window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   deferredPrompt = e;
@@ -57,6 +89,17 @@ window.addEventListener('appinstalled', (e) => {
 });
 window.addEventListener('load', async () => {
   try {
+    MAIN = document.querySelector('#main');
+    MODAL_POST = document.querySelector('#modal-post-section');
+    const urlParams = new URLSearchParams(window.location.search);
+    const query = urlParams.get('q');
+    if (query) {
+      // Recuperando todos los registros de firebase
+      const allPosts = await db.collection('posts').orderBy('timestamp', 'desc').get();
+      allPosts.forEach(post => {
+        createPosts(post.data());
+      });
+    }
     window.Message = (option = 'success', container = document.querySelector('#toast-container')) => {
       container.classList.remove('success');
       container.classList.remove('error');
@@ -67,8 +110,6 @@ window.addEventListener('load', async () => {
     btnShowPost.addEventListener('click', showPostModal);
     const btnPostCancel = document.querySelector('#btn-post-cancel');
     const btnPostSubmit = document.querySelector('#btn-post-submit');
-    MAIN = document.querySelector('#main');
-    MODAL_POST = document.querySelector('#modal-post-section');
     btnPostCancel.addEventListener('click', closePostModal);
     btnPostSubmit.addEventListener('click', sendData);
     if ('serviceWorker' in navigator) {
